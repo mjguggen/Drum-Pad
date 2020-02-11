71574109c9f48396de7cf9a0d2fefcde10e9c6b0
mjguggen <mjguggen@gmail.com>
2020-02-11 14:34:26 -0600

    Updates

---
+++ precache-manifest.ae9a4bba346019b099e726c9e9b2235c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd387cad46ad057130be9ff00c33b95d",
+    "url": "/Drum-Pad/index.html"
+  },
+  {
+    "revision": "e32031d81569b6450dc3",
+    "url": "/Drum-Pad/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "61d794eb91cedb15e0b3",
+    "url": "/Drum-Pad/static/css/main.1fa8adfe.chunk.css"
+  },
+  {
+    "revision": "e32031d81569b6450dc3",
+    "url": "/Drum-Pad/static/js/2.e0e27232.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Drum-Pad/static/js/2.e0e27232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61d794eb91cedb15e0b3",
+    "url": "/Drum-Pad/static/js/main.91f0c8be.chunk.js"
+  },
+  {
+    "revision": "2b7681878e4e7c061073",
+    "url": "/Drum-Pad/static/js/runtime-main.c9d6dcb8.js"
+  },
+  {
+    "revision": "8481c0480a8e25e9663281ca3b2f48e5",
+    "url": "/Drum-Pad/static/media/1-work-it.8481c048.wav"
+  },
+  {
+    "revision": "9d042fe1b5b78afcf76661d8b7dcc53e",
+    "url": "/Drum-Pad/static/media/10-hour.9d042fe1.wav"
+  },
+  {
+    "revision": "12d8e81923dea51dffb816dabe03f789",
+    "url": "/Drum-Pad/static/media/11-our.12d8e819.wav"
+  },
+  {
+    "revision": "fa6e513137aa2983ace41c106281ff4d",
+    "url": "/Drum-Pad/static/media/12-never.fa6e5131.wav"
+  },
+  {
+    "revision": "2bab9d0049e2847318dc8d5e2433ec26",
+    "url": "/Drum-Pad/static/media/13-ever.2bab9d00.wav"
+  },
+  {
+    "revision": "227faaa405e0e3d1cec588eb2fb5d4f3",
+    "url": "/Drum-Pad/static/media/14-after.227faaa4.wav"
+  },
+  {
+    "revision": "056aaa3ff811f1211e9520de8ca3bace",
+    "url": "/Drum-Pad/static/media/15-work-is.056aaa3f.wav"
+  },
+  {
+    "revision": "665fb4d75ca616fab8eee9e776df9ced",
+    "url": "/Drum-Pad/static/media/16-over.665fb4d7.wav"
+  },
+  {
+    "revision": "a019b56785624ad5a62ff3c2ab085fd2",
+    "url": "/Drum-Pad/static/media/2-make-it.a019b567.wav"
+  },
+  {
+    "revision": "6099320f62ab303c04f0ad4df44750b0",
+    "url": "/Drum-Pad/static/media/3-do-it.6099320f.wav"
+  },
+  {
+    "revision": "7b15c15faa220b052ee7702b42fe05a1",
+    "url": "/Drum-Pad/static/media/4-makes-us.7b15c15f.wav"
+  },
+  {
+    "revision": "8b14d3a307480155fba46060849c3d90",
+    "url": "/Drum-Pad/static/media/5-harder.8b14d3a3.wav"
+  },
+  {
+    "revision": "b1e2efde16a6f6d1d4c610647c5cec56",
+    "url": "/Drum-Pad/static/media/6-better.b1e2efde.wav"
+  },
+  {
+    "revision": "26afdf0a84a47bea842c8cb42fdee6e2",
+    "url": "/Drum-Pad/static/media/7-faster.26afdf0a.wav"
+  },
+  {
+    "revision": "e1c6c17361c56be723bed57b9a9d086f",
+    "url": "/Drum-Pad/static/media/8-stronger.e1c6c173.wav"
+  },
+  {
+    "revision": "ca872d7473a9e7ea315c527c1a9166f7",
+    "url": "/Drum-Pad/static/media/9-more-than.ca872d74.wav"
+  },
+  {
+    "revision": "7b012d68881e38d3c8fab7629c65c50a",
+    "url": "/Drum-Pad/static/media/mpd-small.7b012d68.jpg"
+  }
+]);
